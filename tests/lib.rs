8ca9cd056bb532180834b553245b16e618769d82
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-09 20:03:59 -0400

    Update the tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -12,9 +12,9 @@ fn new() {
 
     assert_eq!(circuit.cores, 2);
     assert_eq!(circuit.nodes, 20);
-    assert::within(&circuit.capacitance, &fixture::C, 1e-13);
-    assert::within(&circuit.capacitance, &fixture::C, 1e-13);
-    assert::within(&circuit.conductance, &fixture::G, 1e-13);
+    assert::within(&circuit.capacitance[..], &fixture::C[..], 1e-13);
+    assert::within(&circuit.capacitance[..], &fixture::C[..], 1e-13);
+    assert::within(&circuit.conductance[..], &fixture::G[..], 1e-13);
 }
 
 #[test]
@@ -22,6 +22,6 @@ fn new_with_params() {
     let (floorplan, config) = (fixture::find("002.flp"), fixture::find("hotspot.config"));
     let circuit = Circuit::new(&floorplan, &config, "t_chip 0.00042 k_chip 42").unwrap();
 
-    assert::within(&circuit.capacitance, &fixture::C42, 1e-13);
-    assert::within(&circuit.conductance, &fixture::G42, 1e-13);
+    assert::within(&circuit.capacitance[..], &fixture::C42[..], 1e-13);
+    assert::within(&circuit.conductance[..], &fixture::G42[..], 1e-13);
 }
