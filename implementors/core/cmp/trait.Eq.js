4edade4df2dfc5eaacabe4cba8f863da91ac2ce6
Travis CI <>
2016-06-17 11:51:17 +0000

    Update the documentation

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = [];implementors["matrix"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='matrix/format/compressed/enum.Variant.html' title='matrix::format::compressed::Variant'>Variant</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='matrix/format/packed/enum.Variant.html' title='matrix::format::packed::Variant'>Variant</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='matrix/struct.Error.html' title='matrix::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["matrix"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='matrix/struct.Error.html' title='matrix::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
